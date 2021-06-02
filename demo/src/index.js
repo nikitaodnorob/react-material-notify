535f6c56859072689c88817b4f17e103a8e2e993
nikitaodnorob <nikitaodnorob@gmail.com>
2021-06-02 23:30:12 +0300

    added simple demo

---
+++ demo/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {NotificationContainer} from 'react-material-notify';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <NotificationContainer>
+            <App/>
+        </NotificationContainer>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
