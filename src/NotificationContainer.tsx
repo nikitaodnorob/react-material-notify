bec31f3d4487f2e0cd789ccd7c3ab880775d5795
nikitaodnorob <nikitaodnorob@gmail.com>
2021-06-06 17:27:05 +0300

    moved reducer

--- src/NotificationContainer.tsx
+++ src/NotificationContainer.tsx
@@ -1,21 +1,8 @@
 import React, {FC, useReducer} from 'react';
 import {Notification} from './Notification';
-import {ActionKind, ActionType, NotificationType} from './types';
-import {NotificationContext} from './context';
+import {initialState, NotificationContext, reducer} from './context';
 
 import './styles/NotificationContainer.css';
-
-const initialState: NotificationType[] = [];
-
-const reducer = (state: NotificationType[], action: ActionType) => {
-    if (action.type === ActionKind.ShowNotification) {
-        return state.concat(action.payload);
-    } else if (action.type === ActionKind.CloseNotification) {
-        return state.filter((notification) => notification.id !== action.payload);
-    } else {
-        return state;
-    }
-}
 
 export const NotificationContainer: FC = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, initialState);
--- src/context.ts
+++ src/context.ts
@@ -1,5 +1,17 @@
 import React from 'react';
-import {NotificationContextType} from './types';
+import {ActionKind, ActionType, NotificationContextType, NotificationType} from './types';
 
 export const NotificationContext = React.createContext<NotificationContextType | null>(null);
+
+export const initialState: NotificationType[] = [];
+
+export const reducer = (state: NotificationType[], action: ActionType) => {
+    if (action.type === ActionKind.ShowNotification) {
+        return state.concat(action.payload);
+    } else if (action.type === ActionKind.CloseNotification) {
+        return state.filter((notification) => notification.id !== action.payload);
+    } else {
+        return state;
+    }
+}
 
