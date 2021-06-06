387e627e5451eeb7a413061337843a40fcdbd0ae
nikitaodnorob <nikitaodnorob@gmail.com>
2021-06-06 23:34:46 +0300

    Fixed notification styles

--- src/Notification/Notification.tsx
+++ src/Notification/Notification.tsx
@@ -1,9 +1,10 @@
 import React, { FC } from 'react';
 import Alert from '@material-ui/lab/Alert';
 import { NotificationType } from '../types';
+import styles from './Notification.module.css';
 
 export const Notification: FC<NotificationPropsType> = ({ notification }) => (
-  <Alert severity={notification.level} variant="filled" className="react-material-notify__notification">
+  <Alert severity={notification.level} variant="filled" className={styles.notification}>
     {notification.message}
   </Alert>
 );
